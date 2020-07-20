75d851718ff833953fef39980db5d3a4ad005145
Tom Hudson <tom.hudson.gamedev@gmail.com>
2020-07-20 11:10:26 +0100

    Init Comm
    My initial commit, set up a basic hello world console app

---
+++ String To Char Prac/String To Char Prac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace String_To_Char_Prac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
