19a0f1cdd9a3cce952cb0668285727556b03e9f1
Tom Hudson <tom.hudson.gamedev@gmail.com>
2020-07-20 11:30:20 +0100

    Done
    The simple if else statements work but the ternary statement doesn't do the correct effect.

--- String To Char Prac/String To Char Prac/Program.cs
+++ String To Char Prac/String To Char Prac/Program.cs
@@ -6,7 +6,33 @@ namespace String_To_Char_Prac
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             * Aim is to print out the str string as a char array and have alternating letters be spaced in for a cool console effect
+             */
+            string str = "Hello World!";
+            char[] charArr = str.ToCharArray();
+            bool space = false;
+            foreach (char c in charArr)
+            {
+                /* Wanted to try getting it into less lines of code but couldn't get the cascading effect I wanted 
+                string s = (space != true) ? s = " " + c : s = c.ToString();
+                Console.WriteLine(s);
+                space = true;
+                */
+#if true
+                if (space)
+                {
+                    Console.WriteLine(" " + c);
+                    space = false;
+                }
+                else
+                {
+                    Console.WriteLine(c);
+                    space = true;
+                }
+#endif
+                
+            }
         }
     }
 }
